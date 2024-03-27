d5fb946670e21e84a8f3e730a87243429e468370
Nahuel Coronel <nahucarc2@gmail.com>
2024-03-27 19:22:37 -0300

    Se agrego el ingresado de parametros por consola

--- tp1-ruleta/main.py
+++ tp1-ruleta/main.py
@@ -1,28 +1,70 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-#CTES
+from sys import argv
+
+
+
+#EL primer argumento es el nombre del propio archivo, por eso lo omitimos
+argumentos_terminal = argv[1:]
+
+if len(argumentos_terminal) != 3:
+  print("""Se deben ingresar 3 parametros de terminal.el comando debe tener la siguiente forma
+python3 tp1-ruleta/main.py  XXX  YYY  ZZZ).
+Donde
+  - XXX -> Cantidad de tiradas a la ruleta por corrida
+  - YYY -> Cantidad de corridas
+  - ZZ -> NUmero elegido""")
+  exit()
+
+
+#Chequeamos que todos los argumentos ingresados sean numericos
+if not all([arg.isnumeric() for arg in argumentos_terminal]):
+  print("Solo se pueden ingresar argumentos numericos")
+  exit()
+#CTES DE VALORES ESPERADS
 FREQ_REL_ESPERADA=1/37
 PROMEDIO_ESPERADO = np.mean(range(37))
-varianza_esperada = np.var(range(37))
-desvio_esperado = np.std(range(37))
+VARIANZA_ESPERADA = np.var(range(37))
+DESVIO_ESPERADO = np.std(range(37))
 
-#VARIABLES
-cantidad_tiradas = 100
-corridas = 3
-numero_elegido= 3
+#CTES INGRASADAS POR TERMINAL
+CANTIDAD_TIRADAS = int(argumentos_terminal[0])
+CORRIDAS = int(argumentos_terminal[1])
+NUMERO_ELEGIDO= int(argumentos_terminal[2])
 
-for j in range(corridas):
+#Chequeamos que las cantidades no sean negativas
+if CANTIDAD_TIRADAS <0 or CORRIDAS<0:
+  print("Tanto la cantidad de tiradas como las corridas deben ser positivas o 0")
+
+#Chequeamos que el numero elegido lo supere los valores posibles
+if NUMERO_ELEGIDO>36:
+  print("El numero elegido debe estar entre 0 y 36")
+  exit()
+
+
+
+
+
+print(f"""cantidad_tiradas = {CANTIDAD_TIRADAS}
+corridas={CORRIDAS}
+numero_elegido={NUMERO_ELEGIDO}\n""")
+
+
+
+
+
+for j in range(CORRIDAS):
   promedio_historico = []
   freq_rel_historico = []
   varianza_historico=[]
   desvio_historico=[]
   valores = []
-  for i in range(cantidad_tiradas):
+  for i in range(CANTIDAD_TIRADAS):
     valor_actual = random.randint(0,36)
     valores.append(valor_actual)
 
-    freq_abs_actual = valores.count(numero_elegido)
+    freq_abs_actual = valores.count(NUMERO_ELEGIDO)
     freq_rel_actual= freq_abs_actual/(i+1)
     freq_rel_historico.append(freq_rel_actual)
 
@@ -58,13 +100,13 @@ for j in range(corridas):
   plt.title("desvio historico")
   plt.xlabel("n(nro tiradas)")
   plt.ylabel("vd(valor del desvio)")
-  plt.axhline(y=desvio_esperado, color='r', linestyle='-.')
+  plt.axhline(y=DESVIO_ESPERADO, color='r', linestyle='-.')
 
   plt.subplot(2, 2, 4)
   plt.plot(varianza_historico,label="varianza historico")
   plt.title("varainza historico")
   plt.xlabel("n(nro tiradas)")
   plt.ylabel("vv(valor de la varianza)")
-  plt.axhline(y=varianza_esperada, color='r', linestyle='-.')
+  plt.axhline(y=VARIANZA_ESPERADA, color='r', linestyle='-.')
   plt.tight_layout()
   plt.show()
